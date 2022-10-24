5ca45a10c0706d71f9b675d3908993e4c61e49c4
nicofishman <nicourifishman@gmail.com>
2022-10-24 07:57:49 -0300

    tailwind

--- App.tsx
+++ App.tsx
@@ -1,12 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { styled } from 'nativewind';
+import * as Contacts from 'expo-contacts';
+import { useEffect } from 'react';
+
+const StyledView = styled(View)
+const StyledText = styled(Text)
 
 export default function App() {
+    useEffect(() => {
+        (async () => {
+          const { status } = await Contacts.requestPermissionsAsync();
+          if (status === 'granted') {
+            const { data } = await Contacts.getContactsAsync({
+                fields: [
+                    Contacts.PHONE_NUMBERS,
+                    Contacts.EMAILS,
+                ],
+            });
+            
+    
+            if (data.length > 0) {
+              const contact = data[0];
+              console.log(contact);
+            }
+          }
+        })();
+      }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <StyledView className="flex-1 items-center justify-center bg-amber-400">
+      <StyledText>Open up App.tsx to start working on your app!</StyledText>
       <StatusBar style="auto" />
-    </View>
+    </StyledView>
   );
 }
 
